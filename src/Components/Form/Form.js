9c1e25c8c6f38e9ce60a75e44deea49bc7b3653a
Jordan <kingjordan2122@gmail.com>
2022-06-30 13:53:08 -0400

    updated maxLength on name and Home

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -9,13 +9,13 @@ export default function Form({ errorMessage, onChangeName, submit }) {
         <div className='froum'>
             <div className='forum-inputs'>
                 <div className="label">Name:</div >
-                <input name='name' type="text" className="input-field" placeholder="Name" defaultValue={''} onChange={onChangeName}></input>
+                <input name='name' type="text" className="input-field" placeholder="Name" defaultValue={''} onChange={onChangeName} maxLength={50}></input>
                 <div className="label">Phone Number:</div>
                 <input name='phone' type="text" className="input-field" placeholder="000-000-0000" defaultValue={''} onChange={onChangeName} maxLength={12}></input>
                 <div className="label">Birth Day:</div>
                 <input name='b_day' type="text" className="input-field" placeholder="MM/DD/YYYY" defaultValue={''} onChange={onChangeName} maxLength={10}></input>
                 <div className="label">Home Address:</div>
-                <input name='address' type="text" className="input-field" placeholder="Home Address" defaultValue={''} onChange={onChangeName}></input>
+                <input name='address' type="text" className="input-field" placeholder="Home Address" defaultValue={''} onChange={onChangeName} maxLength={40}></input>
                 <div className="label">Todays Date:</div>
                 <input name='todays_date' type="text" className="input-field" placeholder="MM/DD/YYYY" defaultValue={''} onChange={onChangeName} maxLength={10}></input>
                 <div className="errorMessage">{errorMessage}</div>
